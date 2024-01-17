881864b2eae3b170e56a304a60a77c47dd0aa5c7
anna17rom <annromanovskaya24@gmail.com>
2024-01-17 22:07:58 +0100

    connection

---
+++ src/main/java/org/example/GoClientV2.java
@@ -0,0 +1,95 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.ServerResponse;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
+
+public class GoClientV2 {
+
+    private final static ObjectMapper MAPPER = new ObjectMapper();
+
+
+    public static void main(String... args) {
+
+        try {
+            try (Socket socket = new Socket("localhost", 8000)) {
+
+                DataInputStream is = new DataInputStream(socket.getInputStream());
+                DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+
+                CountDownLatch latch = new CountDownLatch(0);
+                ClientSession session = new ClientSession(s -> {
+                    try {
+                        os.writeUTF(MAPPER.writeValueAsString(s));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+                new Thread(() -> {
+                    while (true) {
+                        try {
+                            ServerResponse resp = MAPPER.readValue(is.readUTF(), ServerResponse.class);
+                            session.handleResponse(resp);
+                            latch.countDown();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }).start();
+
+                session.start();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/org/example/GoServerV2.java
@@ -0,0 +1,110 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.ServerResponse;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Consumer;
+
+public class GoServerV2 {
+
+    private static final int PORT = 8000;
+
+    private final static ObjectMapper MAPPER = new ObjectMapper();
+
+
+    public static void main(String... args) throws IOException {
+
+        do {
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                System.out.println("Waiting for 1st player to connect.");
+                Socket p1Socket = serverSocket.accept();
+                ClientIO p1IO = ClientIO.fromSocket(p1Socket);
+                Player p1 = new RemotePlayer(p1IO);
+                // Wait for 1st player choose game mode
+                ServerResponse.Mode mode = p1.mode();
+                System.out.println("1st player connected");
+                Player p2;
+                if (mode == ServerResponse.Mode.MULTIPLAYER) {
+                    p2 = new RemotePlayer(ClientIO.fromSocket(serverSocket.accept()));
+                } else {
+                    p2 = new CpuPlayer();
+                }
+                p2.mode();
+                System.out.println("2nd player connected");
+                ServerSession session = new ServerSession(p1, p2);
+                session.start();
+            } catch (Exception e) {
+                System.out.println("Session ended with error: " + e.getMessage());
+            }
+        } while (true);
+    }
+
+    private static void listenForPlayerRequest(ClientIO playerIO, Consumer<ClientRequest> consumer) {
+        new Thread(() -> {
+            try {
+                String line = playerIO.reader.readUTF();
+                ClientRequest req = MAPPER.readValue(line, ClientRequest.class);
+                consumer.accept(req);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    public static class ClientIO {
+
+        final DataInputStream reader;
+        final DataOutputStream writer;
+
+        private ClientIO(DataInputStream reader, DataOutputStream writer) {
+            this.reader = reader;
+            this.writer = writer;
+        }
+
+        public static ClientIO fromSocket(Socket socket) throws IOException {
+            return new ClientIO(
+                    new DataInputStream(socket.getInputStream()),
+                    new DataOutputStream(socket.getOutputStream())
+            );
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
